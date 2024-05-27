4d6f8382a6ce8b8be4551aa6fd26a9954b7ff7d5
Estevam <estevamsouzalaureth@gmail.com>
2024-05-26 22:40:34 -0300

    Update S16E09.ino

--- server/S16E09/S16E09.ino
+++ server/S16E09/S16E09.ino
@@ -1,34 +1,31 @@
-#include "DHT.h"
-#include <WiFi.h>
-#include <PubSubClient.h>
 #include <Wire.h>
 #include <hd44780.h>                        // main hd44780 header
 #include <hd44780ioClass/hd44780_I2Cexp.h>  // i2c expander i/o class header
+#include "DHT.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
 
 // Parametros de conexão WiFi e MQTT
 const char* ssid = "Penelopecharmosa"; // REDE
 const char* password = "13275274"; // SENHA
-
 const char* mqtt_broker = "b37.mqtt.one"; // Host do broker
 const char* topic = "2bqsvw6678/"; // Tópico a ser subscrito e publicado
 const char* mqtt_username = "2bqsvw6678"; // Usuário
 const char* mqtt_password = "0efiqruwxy"; // Senha
 const int mqtt_port = 1883; // Porta
 
-// Pinos
-const int relayPin = 52; // Pino do relé
-const int dhtPin = 0; // Pino de dados do DHT11
-const int waterSensorPin = A0;  // Pino analógico onde o sensor de água está conectado
-const int numReadings = 10;  // Número de leituras para fazer a média
-const int contrastPin = A2;     // Pino do potenciômetro para controlar o contraste do LCD
-const int buttonPin = 13;       // Pino do botão
-
-// Pinos dos LEDs
-const int redLedPin = 4;       // LED vermelho
-const int yellowLedPin = 3;    // LED amarelo
-const int greenLedPin = 2;     // LED verde
+#define BUTTON_PIN 13       // Pino do botão
+#define DHTPIN 0 // Pino de dados do DHT11
+#define DHTTYPE DHT11       // Tipo de sensor DHT (DHT11 ou DHT22)
+#define WATER_SENSOR_PIN A0  // Pino analógico onde o sensor de água está conectado
+#define CONTRAST_PIN A2     // Pino do potenciômetro para controlar o contraste do LCD
+#define RED_LED_PIN 4       // LED vermelho
+#define YELLOW_LED_PIN 3    // LED amarelo
+#define GREEN_LED_PIN 2     // LED verde
+#define RELAY_PIN 52 // Pino do relé
 
 // Variáveis de Leitura de Água
+const int numReadings = 10;  // Número de leituras para fazer a média
 int readings[numReadings];   // Armazena as leituras analógicas
 int readIndex = 0;           // Índice da leitura atual
 int total = 0;               // Soma das leituras
@@ -40,17 +37,16 @@ bool relayState = false; // Estado inicial do relé (desligado)
 bool displayTempHum = true; // Variável para alternar entre exibir temperatura e umidade e "Hello World"
 bool displayWaterLevel = false; // Variável para alternar entre exibir nível de água e outras informações
 bool coffeeMakerOn = false; // Variável para indicar o estado da cafeteira
+bool displayHello = false;
 
 // Variáveis de debounce
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
-bool lastButtonState = HIGH;
-bool buttonState = HIGH;
 
 // Objetos
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
-DHT dht(dhtPin, DHT11);
+DHT dht(DHTPIN, DHTTYPE);
 hd44780_I2Cexp lcd;
 
 // Protótipos
@@ -60,13 +56,22 @@ void toggleRelay(bool state);
 void dht_sensor_getdata();
 void water_sensor_getdata();
 void displayCoffeeMakerState();
-void displayTemperatureAndHumidity();
-void displayWaterLevelFunc();
 void displayHelloWorld();
-void controlCoffeeMaker();
 
 void setup() {
   Serial.begin(9600);
+
+  // Inicializa o sensor DHT
+  dht.begin();
+  
+  // Inicializa o LCD
+  lcd.begin(16, 2); // Definindo 16 colunas e 2 linhas
+
+  // Inicializa o pino do botão
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  // Inicializa o pino de controle do contraste do LCD
+  pinMode(CONTRAST_PIN, INPUT);
 
   // Inicialização da conexão WiFi
   WiFi.begin(ssid, password);
@@ -82,28 +87,16 @@ void setup() {
   mqttStatus = connectMQTT();
 
   // Configuração do pino do relé
-  pinMode(relayPin, OUTPUT);
-  digitalWrite(relayPin, LOW); // Desliga o relé inicialmente
-
-  // Inicializa o sensor DHT
-  dht.begin();
-  
-  // Inicializa o LCD
-  lcd.begin(16, 2); // Definindo 16 colunas e 2 linhas
-
-  // Inicializa o pino de controle do contraste do LCD
-  pinMode(contrastPin, INPUT);
-
-  // Inicializa o pino do botão
-  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW); // Desliga o relé inicialmente
 
   // Configura os pinos dos LEDs como saída
-  pinMode(redLedPin, OUTPUT);
-  pinMode(yellowLedPin, OUTPUT);
-  pinMode(greenLedPin, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(YELLOW_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
 
   // Inicializa o sensor de água
-  pinMode(waterSensorPin, INPUT);
+  pinMode(WATER_SENSOR_PIN, INPUT);
   delay(100);
   
   // Inicializa todas as leituras com 0
@@ -112,7 +105,7 @@ void setup() {
   }
 
   // Exibe a temperatura e a umidade no LCD
-  displayTemperatureAndHumidity();
+  dht_sensor_getdata();
 }
 
 void loop() {
@@ -128,52 +121,38 @@ void loop() {
     client.loop();
   }
 
-  // Leitura do botão com debounce
-  bool reading = digitalRead(buttonPin);
-
   // Verifica se o botão foi pressionado
-  if (reading == LOW) {
-    // Inverte o estado do modo de exibição
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    // Alterna entre os modos de exibição
     if (displayTempHum) {
       displayTempHum = false;
       displayWaterLevel = true;
+      displayHello = false;
     } else if (displayWaterLevel) {
       displayWaterLevel = false;
-      coffeeMakerOn = !coffeeMakerOn; // Inverte o estado da cafeteira
-      controlCoffeeMaker();
+      displayHello = true;
     } else {
       displayTempHum = true;
+      displayHello = false;
     }
 
     // Exibe com base no estado do modo de exibição
     if (displayTempHum) {
-      displayTemperatureAndHumidity();
-        // Aguarda até que o botão seja liberado
-        while (digitalRead(buttonPin) == LOW) {
-          delay(10);
-      }
+      dht_sensor_getdata();
     } else if (displayWaterLevel) {
-      displayWaterLevelFunc();
-        // Aguarda até que o botão seja liberado
-        while (digitalRead(buttonPin) == LOW) {
-          delay(10);
-      }
+      water_sensor_getdata();
     } else {
-      displayHelloWorld();
-        // Aguarda até que o botão seja liberado
-        while (digitalRead(buttonPin) == LOW) {
-          delay(10);
-      }
+      displayHelloWorld(); // Chama a função para exibir "Hello World"
+    }
+
+    // Aguarda até que o botão seja liberado
+    while (digitalRead(BUTTON_PIN) == LOW) {
+      delay(10);
     }
   }
 
-  // Mensagem de debug
-  Serial.println("Botão pressionado, estado atualizado.");
-
-  lastButtonState = reading;
-
   // Adiciona um pequeno delay para evitar leituras muito frequentes
-  delay(500);
+  delay(1000);
 }
 
 bool connectMQTT() {
@@ -217,7 +196,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 void toggleRelay(bool state) {
   relayState = state;
-  digitalWrite(relayPin, state ? HIGH : LOW);
+  digitalWrite(RELAY_PIN, state ? HIGH : LOW);
   client.publish(topic, state ? "ligado" : "desligado");
 }
 
@@ -229,11 +208,23 @@ void dht_sensor_getdata() {
   Serial.print("Temperatura: ");
   Serial.println(temp);
 
+  // Exibe a temperatura e umidade no LCD
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temp);
+  lcd.print(" C");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Umidade: ");
+  lcd.print(hm);
+  lcd.print(" %");
+
   // Atualiza o LED com base na umidade e temperatura lidas
   if (temp > 30.0) {
-    digitalWrite(greenLedPin, LOW); // Liga o LED se a temperatura for maior que 30 graus
+    digitalWrite(GREEN_LED_PIN, LOW); // Liga o LED se a temperatura for maior que 30 graus
   } else {
-    digitalWrite(greenLedPin, HIGH); // Desliga o LED se a temperatura for menor ou igual a 30 graus
+    digitalWrite(GREEN_LED_PIN, HIGH); // Desliga o LED se a temperatura for menor ou igual a 30 graus
   }
 
   // Converte os valores para String
@@ -248,7 +239,7 @@ void dht_sensor_getdata() {
 void water_sensor_getdata() {
   // Remove a leitura mais antiga e adiciona a nova leitura
   total -= readings[readIndex];
-  readings[readIndex] = analogRead(waterSensorPin);
+  readings[readIndex] = analogRead(WATER_SENSOR_PIN);
   total += readings[readIndex];
   readIndex = (readIndex + 1) % numReadings;
 
@@ -258,12 +249,34 @@ void water_sensor_getdata() {
   // Converte o valor da média para um valor percentual
   float waterPercentage = (average / 1023.0) * 100;
 
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Nivel de Agua:");
+  lcd.setCursor(0, 1);
+  lcd.print(waterPercentage);
+  lcd.print(" %");
+
   // Envia a média das leituras para o monitor serial
   Serial.print("Nível de Água (%): ");
   Serial.println(waterPercentage);
 
   // Converte o valor para String
   String waterString = String(waterPercentage, 2);
+
+  // Controle dos LEDs baseado no nível de água
+  if (waterPercentage >= 0 && waterPercentage <= 25) {
+    digitalWrite(RED_LED_PIN, HIGH);
+    digitalWrite(YELLOW_LED_PIN, LOW);
+    digitalWrite(GREEN_LED_PIN, LOW);
+  } else if (waterPercentage > 25 && waterPercentage <= 50) {
+    digitalWrite(RED_LED_PIN, LOW);
+    digitalWrite(YELLOW_LED_PIN, HIGH);
+    digitalWrite(GREEN_LED_PIN, LOW);
+  } else if (waterPercentage > 50 && waterPercentage <= 100) {
+    digitalWrite(RED_LED_PIN, LOW);
+    digitalWrite(YELLOW_LED_PIN, LOW);
+    digitalWrite(GREEN_LED_PIN, HIGH);
+  }
 
   // Publica o valor no broker MQTT
   client.publish("2bqsvw6678/nivelAgua2505", waterString.c_str());
@@ -286,68 +299,6 @@ void displayCoffeeMakerState() {
   }
 }
 
-void displayTemperatureAndHumidity() {
-  // Realiza a leitura da temperatura e umidade do sensor DHT
-  float temperature = dht.readTemperature(); // Lê a temperatura em Celsius
-  float humidity = dht.readHumidity();       // Lê a umidade relativa do ar
-
-  // Exibe a temperatura e umidade no LCD
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Temp: ");
-  lcd.print(temperature);
-  lcd.print(" C");
-
-  lcd.setCursor(0, 1);
-  lcd.print("Umidade: ");
-  lcd.print(humidity);
-  lcd.print(" %");
-
-  // Exibe a temperatura e umidade no Monitor Serial para debug
-  Serial.print("Temperatura: ");
-  Serial.print(temperature);
-  Serial.println(" C");
-  Serial.print("Umidade: ");
-  Serial.print(humidity);
-  Serial.println(" %");
-}
-
-void displayWaterLevelFunc() {
-  // Lê o valor do sensor de nível de água
-  int waterLevel = analogRead(waterSensorPin);
-
-  // Converte o valor lido em um nível de água (porcentagem)
-  float waterLevelPercent = (waterLevel / 1023.0) * 100.0;
-
-  // Exibe o nível de água no LCD
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Nivel de Agua:");
-  lcd.setCursor(0, 1);
-  lcd.print(waterLevelPercent);
-  lcd.print(" %");
-
-  // Exibe o nível de água no Monitor Serial para debug
-  Serial.print("Nivel de Agua: ");
-  Serial.print(waterLevelPercent);
-  Serial.println(" %");
-
-  // Controle dos LEDs baseado no nível de água
-  if (waterLevelPercent >= 0 && waterLevelPercent <= 25) {
-    digitalWrite(redLedPin, HIGH);
-    digitalWrite(yellowLedPin, LOW);
-    digitalWrite(greenLedPin, LOW);
-  } else if (waterLevelPercent > 25 && waterLevelPercent <= 50) {
-    digitalWrite(redLedPin, LOW);
-    digitalWrite(yellowLedPin, HIGH);
-    digitalWrite(greenLedPin, LOW);
-  } else if (waterLevelPercent > 50 && waterLevelPercent <= 100) {
-    digitalWrite(redLedPin, LOW);
-    digitalWrite(yellowLedPin, LOW);
-    digitalWrite(greenLedPin, HIGH);
-  }
-}
-
 void displayHelloWorld() {
   // Exibe "Hello World" no LCD
   lcd.clear();
@@ -355,18 +306,10 @@ void displayHelloWorld() {
   lcd.print("Hoje tem cafe?");
 
   // Apaga todos os LEDs quando exibindo "Hello World"
-  digitalWrite(redLedPin, LOW);
-  digitalWrite(yellowLedPin, LOW);
-  digitalWrite(greenLedPin, LOW);
+  digitalWrite(RED_LED_PIN, LOW);
+  digitalWrite(YELLOW_LED_PIN, LOW);
+  digitalWrite(GREEN_LED_PIN, LOW);
 
   // Exibe o estado da cafeteira
   displayCoffeeMakerState();
-}
-
-void controlCoffeeMaker() {
-  if (coffeeMakerOn) {
-    digitalWrite(relayPin, HIGH); // Liga a cafeteira
-  } else {
-    digitalWrite(relayPin, LOW);  // Desliga a cafeteira
-  }
 }
